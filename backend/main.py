c0f581560eb26920b77928dd35291dccef2a01f7
wisetree228 <pirumovgeorgij@gmail.com>
2025-03-13 15:50:07 +0300

    Сделал создание голосов

---
+++ backend/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from backend.application.route import router
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Разрешить все домены (для разработки)
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(router)
+
