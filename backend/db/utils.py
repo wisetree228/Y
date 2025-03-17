7af157a997cdda6dfbfdcce79cd8d2600688fcbf
wisetree228 <pirumovgeorgij@gmail.com>
2025-03-17 14:20:20 +0300

    Получение картинок поста сделано

--- backend/db/utils.py
+++ backend/db/utils.py
@@ -31,19 +31,9 @@ async def get_user_by_username(username: str, db: Session):
     return result_email.scalars().first()
 
 
-async def get_user_by_id(id: int, db: Session):
-    result_db = await db.execute(select(User).filter(User.id == id))
-    return result_db.scalars().first()
-
-
 async def delete_object(object: Union[User, Post, Friendship, FriendshipRequest, VotingVariant, Like, Message, Vote], db: Session):
     await db.delete(object)
     await db.commit()
-
-
-async def get_post_by_id(id: int, db: Session):
-    result_db = await db.execute(select(Post).filter(Post.id == id))
-    return result_db.scalars().first()
 
 
 async def get_like_on_post_from_user(post_id: int, user_id: int, db: Session):
@@ -58,11 +48,6 @@ async def get_likes_count(post_id: int, db: Session):
     count_query = await db.execute(select(func.count()).select_from(Like).filter(Like.post_id==post_id))
     count = count_query.scalar()
     return count
-
-
-async def get_variant_by_id(var_id: int, db: Session):
-    result_db = await db.execute(select(VotingVariant).filter(VotingVariant.id == var_id))
-    return result_db.scalars().first()
 
 
 async def get_user_vote(var_id: int, user_id: int, db: Session):
@@ -148,3 +133,6 @@ async def get_images_id_for_post(post_id: int, db: Session):
     return id_list
 
 
+async def get_object_by_id(object_type: Union[Type[User], Type[Post], Type[Friendship], Type[FriendshipRequest], Type[VotingVariant], Type[Like], Type[Message], Type[Vote], Type[MediaInPost]], id: int, db: Session):
+    result = await db.execute(select(object_type).filter(object_type.id==id))
+    return result.scalars().first()
