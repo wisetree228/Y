968c4cb35322ca51bbd84ba018c9de74567f5208
Pavel Shemakhanov <Spacepilot2008@gmail.com>
2025-04-18 17:40:05 +0300

    start working on complaints

--- backend/application/routes.py
+++ backend/application/routes.py
@@ -443,3 +443,31 @@ async def get_votes(voting_variant_id: int, user_id: str = Depends(get_current_u
         json - список голосовавших
     """
     return await get_votes_view(voting_variant_id = voting_variant_id, user_id = int(user_id), db=db)
+
+
+@router.get('/complaint_post/{post_id}', dependencies=[Depends(security.access_token_required)])
+async def complaint_post(post_id: int, user_id: str = Depends(get_current_user_id), db: Session = Depends(get_db)):
+    """
+    Возвращает список голосовавших за вариант голосования в посте
+    Args:
+        post_id (int): id поста 
+        user_id (str): id пользователя
+        db (Session): сессия бд
+    Returns:
+        json - статус операции
+    """
+    return await complaint_post_view(post_id = post_id, user_id = int(user_id), db=db)
+
+
+@router.get('/complaint_comment/{comment_id}', dependencies=[Depends(security.access_token_required)])
+async def complaint_comment(comment_id: int, user_id: str = Depends(get_current_user_id), db: Session = Depends(get_db)):
+    """
+    Возвращает список голосовавших за вариант голосования в посте
+    Args:
+        comment_id (int): id поста 
+        user_id (str): id пользователя
+        db (Session): сессия бд
+    Returns:
+        json - статус операции
+    """
+    return await complaint_comment_view(comment_id = comment_id, user_id = int(user_id), db=db)
