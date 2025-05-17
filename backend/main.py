71f35abc4cc4cc572d9ea1ae61907ce4eaec15df
Мудрый дуб <pirumovgeorgij@gmail.com>
2025-05-17 15:22:01 +0300

    Сделал CI, изменил requirements.txt чтобы не ломались тесты

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost"],
+    allow_origins=["http://localhost:3000", "http://localhost"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
